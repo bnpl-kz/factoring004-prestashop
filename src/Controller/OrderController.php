1286f255636c185cee85352aa44ff8963c538027
Mirlan Jelamanov <mirlan.jelamanov@gmail.com>
2022-07-21 14:59:35 +0600

    Overrides admin order controller

---
+++ src/Controller/OrderController.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BnplPartners\Factoring004Prestashop\Controller;
+
+class OrderController extends \PrestaShopBundle\Controller\Admin\Sell\Order\OrderController
+{
+
+}

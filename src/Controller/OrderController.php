7e0cb893ad3fb8073a619cf5c0143288fc98b2fb
Mirlan Jelamanov <mirlan.jelamanov@gmail.com>
2022-07-21 17:11:23 +0600

    Adds delivery feature in order detail page

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -23,6 +23,32 @@ class OrderController extends \PrestaShopBundle\Controller\Admin\Sell\Order\Orde
         $this->deliveryHandler = $deliveryHandler;
     }
 
+    public function updateStatusAction(int $orderId, Request $request): RedirectResponse
+    {
+        $orderStatusId = $request->request->get(
+            'update_order_status_action_bar',
+            $request->request->get('update_order_status', [])
+        )['new_order_status_id'];
+
+        try {
+            $shouldConfirmOtp = $this->deliveryHandler->handle($orderId, $orderStatusId);
+        } catch (PackageException $e) {
+            $this->addFlash('error', $this->getErrorMessageForException($e, [
+                PackageException::class => _PS_MODE_DEV_ ? $e->getMessage() : 'An error occurred',
+            ]));
+
+            return $this->redirectToRoute('admin_orders_view', [
+                'orderId' => $orderId,
+            ]);
+        }
+
+        if ($shouldConfirmOtp) {
+            return $this->redirectToRoute('admin_factoring004_otp_index');
+        }
+
+        return parent::updateStatusAction($orderId, $request);
+    }
+
     public function updateStatusFromListAction(int $orderId, Request $request): RedirectResponse
     {
         try {

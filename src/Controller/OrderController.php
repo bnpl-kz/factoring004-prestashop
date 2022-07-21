40f8c8f81465919453182b0b1da77a700584a960
Mirlan Jelamanov <mirlan.jelamanov@gmail.com>
2022-07-21 15:04:57 +0600

    Adds delivery feature in orders list page

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -4,7 +4,41 @@ declare(strict_types=1);
 
 namespace BnplPartners\Factoring004Prestashop\Controller;
 
+use BnplPartners\Factoring004\Exception\PackageException;
+use BnplPartners\Factoring004Prestashop\DeliveryHandler;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+
 class OrderController extends \PrestaShopBundle\Controller\Admin\Sell\Order\OrderController
 {
+    /**
+     * @var \BnplPartners\Factoring004Prestashop\DeliveryHandler
+     */
+    private $deliveryHandler;
 
+    public function __construct(DeliveryHandler $deliveryHandler)
+    {
+        parent::__construct();
+
+        $this->deliveryHandler = $deliveryHandler;
+    }
+
+    public function updateStatusFromListAction(int $orderId, Request $request): RedirectResponse
+    {
+        try {
+            $shouldConfirmOtp = $this->deliveryHandler->handle($orderId, $request->request->get('value'));
+        } catch (PackageException $e) {
+            $this->addFlash('error', $this->getErrorMessageForException($e, [
+                PackageException::class => _PS_MODE_DEV_ ? $e->getMessage() : 'An error occurred',
+            ]));
+
+            return $this->redirectToRoute('admin_orders_index');
+        }
+
+        if ($shouldConfirmOtp) {
+            return $this->redirectToRoute('admin_factoring004_otp_index');
+        }
+
+        return parent::updateStatusFromListAction($orderId, $request);
+    }
 }
